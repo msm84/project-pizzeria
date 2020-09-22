98535d9645ed56cd6157fb4531d4673dcf307f9d
Michal Sutula <michal.sutula@gmail.com>
2020-09-22 17:17:43 +0200

    Price calculation

--- dist/js/script.js
+++ dist/js/script.js
@@ -62,6 +62,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       console.log('new Product', thisProduct);
     }
@@ -88,19 +90,14 @@
     initAccordion(){
       const thisProduct = this;
       /* find the clickable trigger (the element that should react to clicking) */
-      //const trigger = select.menuProduct.clickable;
-      //const trigger = document.querySelector('.product__header');
-
       /* START: click event listener to trigger */
       thisProduct.accordionTrigger.addEventListener('click', function(event){
-        console.log('clicked');
         /* prevent default action for event */
         event.preventDefault();
         /* toggle active class on element of thisProduct */
         thisProduct.element.classList.toggle('active');
         /* find all active products */
         const activeProducts = document.querySelectorAll('.active .products');
-        console.log(activeProducts);
         /* START LOOP: for each active product */
         for (let active of activeProducts){
         /* START: if the active product isn't the element of thisProduct */
@@ -109,11 +106,73 @@
             active.classList.remove('active');
           }
           /* END: if the active product isn't the element of thisProduct */
-
         /* END LOOP: for each active product */
         }
       /* END: click event listener to trigger */
       });
+    }
+    initOrderForm(){
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      console.log('Wywołana metoda initOrderForm');
+    }
+    processOrder(){
+      const thisProduct = this;
+
+      /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+      console.log('Wywołana metoda processOrder');
+      /* set variable price to equal thisProduct.data.price */
+      let price = thisProduct.data.price;
+      /* START LOOP: for each paramId in thisProduct.data.params */
+      for (let paramId in thisProduct.data.params){
+        /* save the element in thisProduct.data.params with key paramId as const param */
+        const param = thisProduct.data.params[paramId];
+        /* START LOOP: for each optionId in param.options */
+        for (let optionId in param.options){
+          /* save the element in param.options with key optionId as const option */
+          const option = param.options[optionId];
+
+          /* find selected options */
+          const optionSelected = formData.hasOwnProperty(paramId) &&
+            formData[paramId].indexOf(optionId) > -1;
+            //console.log(optionSelected);
+            /* START IF: if option is selected and option is not default */
+          if(optionSelected && !option.default){
+            /* add price of option to variable price */
+            price += option.price;
+
+            /* END IF: if option is selected and option is not default */
+          /* START ELSE IF: if option is not selected and option is default */
+          } else if (!optionSelected && option.default){
+            /* deduct price of option from price */
+            price -= option.price;
+            console.log(price);
+            /* END ELSE IF: if option is not selected and option is default */
+          }
+          /* END LOOP: for each optionId in param.options */
+        }
+        /* END LOOP: for each paramId in thisProduct.data.params */
+      }
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
