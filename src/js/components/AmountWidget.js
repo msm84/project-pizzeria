6d8313b81f11971abd77c8d0b32668055a619840
Michal Sutula <michal.sutula@gmail.com>
2020-10-13 20:38:55 +0200

    Date and hour picker

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,66 +1,56 @@
 
 import {select, settings} from '../settings.js';
+import {BaseWidget} from './BaseWidget.js';
 
-export class AmountWidget{
-  constructor(element){
+export class AmountWidget extends BaseWidget {
+  constructor(wrapper) {
+    super(wrapper, settings.amountWidget.defaultValue);
+
     const thisWidget = this;
 
-    thisWidget.getElements(element);
+    thisWidget.getElements();
     thisWidget.initActions();
-    thisWidget.value = settings.amountWidget.defaultValue;
-    thisWidget.setValue(thisWidget.input.value);
-
-    //console.log('AmountWidget:', thisWidget);
-    //console.log('constructor arguments:', element);
-  }
-  getElements(element){
-    const thisWidget = this;
-
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
   }
 
-  setValue(value){
+  getElements() {
     const thisWidget = this;
 
-    const newValue = parseInt(value);
-
-    /* TODO: Add validation */
-
-    //thisWidget.value = newValue;
-    //thisWidget.announce();
-
-    if((thisWidget.value != value) && (newValue >= settings.amountWidget.defaultMin) && (newValue <= settings.amountWidget.defaultMax)) {
-      thisWidget.value = newValue;
-      thisWidget.announce();
-    }
-    thisWidget.input.value = thisWidget.value;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(
+      select.widgets.amount.input
+    );
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(
+      select.widgets.amount.linkDecrease
+    );
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(
+      select.widgets.amount.linkIncrease
+    );
   }
 
-  initActions(){
+  isValid(newValue) {
+    return !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax;
+  }
+
+  initActions() {
     const thisWidget = this;
-    thisWidget.input.addEventListener('change', function () {
-      thisWidget.setValue(thisWidget.input.value);
+
+    thisWidget.dom.input.addEventListener('change', function () {
+      thisWidget.value = thisWidget.dom.input.value;
     });
-    thisWidget.linkDecrease.addEventListener('click', function (event) {
+
+    thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
       event.preventDefault();
-      thisWidget.setValue((thisWidget.value) - 1);
+      thisWidget.value = thisWidget.value - 1;
     });
-    thisWidget.linkIncrease.addEventListener('click', function (event) {
+
+    thisWidget.dom.linkIncrease.addEventListener('click', function (event) {
       event.preventDefault();
-      // console.log(thisWidget.value);
-      thisWidget.setValue((thisWidget.value) + 1);
+      thisWidget.value = thisWidget.value + 1;
     });
   }
 
-  announce(){
+  renderValue() {
     const thisWidget = this;
 
-    const event = new CustomEvent('updated', {
-      bubbles: true//Używamy tutaj innego rodzaju eventu, którego właściwości możemy kontrolować. W tym wypadku włączamy jego właściwość bubbles, dzięki czemu ten event po wykonaniu na jakimś elemencie będzie przekazany jego rodzicowi, oraz rodzicowi rodzica, i tak dalej – aż do samego <body>, document i window.
-    });
-    thisWidget.element.dispatchEvent(event);
+    thisWidget.dom.input.value = thisWidget.value;
   }
 }
--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,32 +1,36 @@
 import { templates, select } from '../settings.js';
-import { utils } from '../utils.js';
 import { AmountWidget } from './AmountWidget.js';
+import { DatePicker } from './DatePicker.js';
+import { HourPicker } from './HourPicker.js';
 
 export class Booking {
-  constructor(element) {
+  constructor(bookingWidgetContainer) {
     const thisBooking = this;
-    thisBooking.render(element);
-    thisBooking.initWidget();
-  }
-  render(element) {
-    const thisBooking = this;
-    const generatedHTML = templates.bookingWidget();
-    thisBooking.dom = {};
 
-    utils.createDOMFromHTML(generatedHTML);
-    thisBooking.dom.wrapper = element;
-    //thisBooking.generatedDOM = utils.createDOMFromHTML(generatedHTML);
-    thisBooking.dom.wrapper.appendChild(utils.createDOMFromHTML(generatedHTML));
-    //thisBooking.dom.peopleAmount = thisBooking.generatedDOM.querySelector(select.booking.peopleAmount);
-    //thisBooking.dom.hoursAmount = thisBooking.generatedDOM.querySelector(select.booking.hoursAmount);
-    //thisBooking.appendChild(thisBooking.generatedDOM);
+    thisBooking.render(bookingWidgetContainer);
+    thisBooking.initWidgets();
+  }
+
+  render(bookingWidgetContainer) {
+    const thisBooking = this;
+
+    const generatedHTML = templates.bookingWidget();
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = bookingWidgetContainer;
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
-  initWidget() {
+
+  initWidgets() {
     const thisBooking = this;
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
